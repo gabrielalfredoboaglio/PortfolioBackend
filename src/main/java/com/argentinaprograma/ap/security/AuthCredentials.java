e41d175385d6a37a10cc852729161cfb2e2735ad
Atixar <gabo_cai_1990@hotmail.com>
2023-01-10 10:41:23 -0300

    backend actualizacion cors yo progamo

---
+++ src/main/java/com/argentinaprograma/ap/security/AuthCredentials.java
@@ -0,0 +1,10 @@
+package com.argentinaprograma.ap.security;
+
+import lombok.Data;
+@Data
+public class AuthCredentials {
+
+        private String email;
+        private String password;
+    }
+
---
+++ src/main/java/com/argentinaprograma/ap/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.argentinaprograma.ap.security;
+
+import com.argentinaprograma.ap.models.Usuario;
+import com.argentinaprograma.ap.repository.UsuarioRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepo usuarioRepo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+       Usuario usuario= usuarioRepo
+                .findOneByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("El usuario con email " + email + "no existe."));
+
+       return new UserDetailsImpl(usuario);
+    }
+}

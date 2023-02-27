d8d03c1e295e403e8d43d5095fcd30b13ed1f092
Gabriel Alfredo Boaglio <109044049+Atixar@users.noreply.github.com>
2023-02-27 20:46:12 -0300

    backend full

--- src/test/java/com/argentinaprograma/ap/ApApplicationTests.java
+++ src/test/java/com/argentinaprograma/ap/ApApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
